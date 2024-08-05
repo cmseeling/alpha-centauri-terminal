b8b76802f1477288527023d7ab382fdb12a0a0b8
chris <cmseeling@gmail.com>
2024-08-04 23:13:24 -0500

    basic notification layout done, still need close button

--- src/components/NotificationBar.svelte
+++ src/components/NotificationBar.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
   import { listen } from '@tauri-apps/api/event'
   import { onMount } from 'svelte';
-  import * as Card from "$lib/components/ui/card";
-  import * as Collapsible from "$lib/components/ui/collapsible";
+
+  import { createCollapsible, melt } from '@melt-ui/svelte';
+  import { slide } from 'svelte/transition';
+  import XmarkSolid from '$lib/components/svgs/xmark-solid.svelte';
+
+  const {
+    elements: { root, content, trigger },
+    states: { open },
+  } = createCollapsible();
 
   interface NotificationEvent {
     level: number;
@@ -51,7 +58,7 @@
       unlisten = unlistenFn
     });
 
-    level = 2;
+    level = 3;
     message = "test"
     details = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sollicitudin velit nisl, id iaculis sapien tristique sed. Aliquam lobortis ut dui et facilisis. Quisque tempus venenatis odio non venenatis. Mauris in libero lobortis, congue augue nec, vehicula metus. Nunc interdum sapien non justo sagittis, et faucibus magna cursus. Nam rutrum maximus orci id accumsan. Mauris fermentum non diam ut elementum. Donec nunc felis, tincidunt in nibh a, ornare tincidunt velit. Etiam non turpis in purus ullamcorper facilisis. Praesent iaculis varius bibendum." 
 
@@ -91,17 +98,26 @@
   <div class="message" style="--textColor: {textColor};">
     {message}
   </div>
+  <div class="close-button-container">
+    <XmarkSolid />
+  </div>
   {#if details !== undefined}
-  <Collapsible.Root>
-    <Collapsible.Trigger><span class="debug-info-trigger">Click to see debugging info.</span></Collapsible.Trigger>
-    <Collapsible.Content>
-      <Card.Root>
-        <Card.Content class="bg-secondary">
-          {details}
-        </Card.Content>
-      </Card.Root>
-    </Collapsible.Content>
-  </Collapsible.Root>
+  <div use:melt={$root} class="root">
+    <div class="header">
+      <button use:melt={$trigger} class="collapse-button">
+        See debugging details
+      </button>
+    </div>
+    {#if $open}
+      <div use:melt={$content} transition:slide>
+        <div class="collapsible">
+          <div class="item">
+            <span>{details}</span>
+          </div>
+        </div>
+      </div>
+    {/if}
+  </div>
   {/if}
 </div>
 {/if}
@@ -111,26 +127,75 @@
     background-color: var(--bgcolor, #c62828);
     position: absolute;
     width: 100%;
+    padding: 0.5em 0;
     display: flex;
     flex-direction: column;
     justify-content: center;
     z-index: 1000;
   }
 
+  .close-button-container {
+    position: absolute;
+    right: 0;
+  }
+
   .message {
     color: var(--textColor, white);
     display: flex;
     justify-content: center;
-    margin: .25em 0;
+    margin: .25em 0 .75em 0;
+  }
+  
+  .root {
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+    max-width: 28rem;
   }
 
-  div[data-collapsible-root] {
+  .header {
     display: flex;
     justify-content: center;
   }
 
-  .debug-info-trigger {
-    color: #1a237e;
-    font-size: 10px;
+  .collapse-button {
+    border-radius: 180px;
+
+    font-size: 0.75rem;
+
+    box-shadow: 0 10px 15px -3px rgb(var(--color-black) / 0.1),
+      0 4px 6px -4px rgb(var(--color-black) / 0.1);
+  }
+
+  .collapse-button:hover {
+    background-color: #bbdefb;
+  }
+
+  .item {
+    margin: 0.5em 0;
+    padding: 0.75rem;
+    border-radius: 0.25rem;
+
+    background-color: #bdbdbd;
+
+    box-shadow: 0 10px 15px -3px rgb(var(--color-black) / 0.1),
+      0 4px 6px -4px rgb(var(--color-black) / 0.1);
+  }
+
+  .item span {
+    font-size: .75rem;
+    line-height: 25px;
+
+    color: rgb(var(--color-magnum-800) / 1);
+  }
+
+  .item::first-of-type {
+    margin: 0.5rem 0;
+  }
+
+  .collapsible {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
   }
 </style>
