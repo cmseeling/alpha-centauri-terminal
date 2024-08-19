f1570caa9c38005508f3509845e2325cc93e2f57
chris <cmseeling@gmail.com>
2024-08-18 21:19:50 -0500

    added playwright and got working test up and running

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -1,13 +1,19 @@
 import { chromium, test, expect } from '@playwright/test';
+import getLogger from './utils/logger';
+
+const logger = getLogger('e2e.log', 'e2e.error.log');
 
 test('terminal loads', async () => {
 	try {
+		logger.info('test starting')
 		const browser = await chromium.connectOverCDP('http://localhost:9222');
 
 		console.log(browser.isConnected() && 'Connected to Chrome.');
 		console.log(`Contexts in CDP session: ${browser.contexts().length}.`);
 
 		const context = browser.contexts()[0];
+		logger.info(context);
+		logger.info(await context.pages().length);
     const page = await context.pages()[0];
 
 		// const page = await context.newPage();
