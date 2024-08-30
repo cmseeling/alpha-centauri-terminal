f51af22ef3f9c3aae9e7306fdcb85a32bd9431ef
chris <cmseeling@gmail.com>
2024-08-30 11:03:39 -0500

    updated configloader tests

--- src/lib/store/ConfigLoader/ConfigLoader.test.ts
+++ src/lib/store/ConfigLoader/ConfigLoader.test.ts
@@ -4,13 +4,13 @@ import { beforeAll, expect, test, vi } from 'vitest';
 import { randomFillSync } from 'crypto';
 import { mockIPC } from '@tauri-apps/api/mocks';
 
-import { TAURI_COMMAND_GET_USER_CONFIG } from '$lib/constants';
-import { isWebGL2Enabled, userConfiguration } from '$lib/store/configurationStore';
+import { TAURI_COMMAND_GET_SYSTEM_INFO, TAURI_COMMAND_GET_USER_CONFIG } from '$lib/constants';
+import { isWebGL2Enabled, systemInfo, userConfiguration } from '$lib/store/configurationStore';
 
 import ConfigLoader from './ConfigLoader.svelte';
 
 const configJson = '{"shell":{"program":"bash","args":[],"env":{},"bell":true},"keymaps":[]}';
-const expectedStore = {
+const expectedConfigStore = {
 	loaded: true,
 	shell: {
 		program: 'bash',
@@ -19,6 +19,11 @@ const expectedStore = {
 		bell: true
 	},
 	keymaps: []
+};
+
+const systemInfoJson = '{"system":"system"}';
+const expectedInfoStore = {
+	system: 'system'
 };
 
 // jsdom doesn't come with a WebCrypto implementation
@@ -32,18 +37,25 @@ beforeAll(() => {
 	});
 });
 
-test('load app configuration', async () => {
+test('load user configuration and system info', async () => {
 	mockIPC((cmd, args) => {
 		if (cmd === TAURI_COMMAND_GET_USER_CONFIG) {
 			return configJson;
 		}
+		if (cmd === TAURI_COMMAND_GET_SYSTEM_INFO) {
+			return systemInfoJson;
+		}
 	});
 
-	const setSpy = vi.spyOn(userConfiguration, 'set');
+	const setUserConfigSpy = vi.spyOn(userConfiguration, 'set');
+	const setSysInfoSpy = vi.spyOn(systemInfo, 'set');
 
 	render(ConfigLoader);
 
 	await waitFor(() => {
-		expect(setSpy).toHaveBeenCalledWith(expectedStore);
+		expect(setUserConfigSpy).toHaveBeenCalledWith(expectedConfigStore);
+	});
+	await waitFor(() => {
+		expect(setSysInfoSpy).toHaveBeenCalledWith(expectedInfoStore);
 	});
 });
