a80f08424b41ec7aa2fcddb949ba89e581103a51
chris <cmseeling@gmail.com>
2024-10-13 22:01:46 -0500

    use font from user config for tab titles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
+  import { userConfiguration } from '$lib/store';
   import ScreenManager from '$components/ScreenManager/ScreenManager.svelte';
 </script>
 
-<div class="h-screen">
+{#if $userConfiguration.loaded}
+<div class="h-screen alpha-centauri" style="--config-font:{$userConfiguration.shell.fonts}">
   <ScreenManager />
 </div>
+{/if}
+
+<style>
+  .alpha-centauri {
+    font-family: var(--config-font);
+  }
+</style>
